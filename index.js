1dab45cfdaabb5a10401e9e88e93112653e01530
Ruben <Ruben>
2026-04-07 09:09:21 +0200

    Arreglo de deslizamiento a la derecha en movil

--- index.js
+++ index.js
@@ -6,20 +6,17 @@ menuHamburguesa.addEventListener('click', () => {
     menuHamburguesa.classList.add('no-activo')
     menuNavegacion.classList.toggle('activo')
     menuHambuguesaCerrar.classList.toggle('activo')
-    document.body.style.overflow = 'hidden'; // Bloqueamos el scroll al abrir el menu
+    document.documentElement.classList.add('bloquear-scroll');
 });
 
 menuHambuguesaCerrar.addEventListener('click', () => {
     menuNavegacion.classList.toggle('activo')
     menuHambuguesaCerrar.classList.toggle('activo')
     menuHamburguesa.classList.remove('no-activo')
-    document.body.style.overflow = 'auto';  // Desbloqueas al cerrar
-
+    document.documentElement.classList.remove('bloquear-scroll');
 });
 
-if (menuNavegacion.classList.contains('activo')) {
-    document.body.style.overflow = 'hidden'
-}
+
 
 
 // ANIMACION DE CONTADOR 
